b175e55c82e8c2a200be4dab6c9f82e870a48d32
Asad <asadshah1066@gmail.com>
2025-06-13 19:15:46 +0530

    cleening code + rendering own component

---
+++ 01basic-vite-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Greeding from "./Greeding.jsx";
+
+function App() {
+  return (
+    <>
+      <h1>React with Vite || Asad </h1>
+      <Greeding />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basic-vite-app/src/Greeding.jsx
@@ -0,0 +1,9 @@
+function Greeding() {
+  return (
+    <div>
+      <h1>Hello asad</h1>
+    </div>
+  );
+}
+
+export default Greeding;
