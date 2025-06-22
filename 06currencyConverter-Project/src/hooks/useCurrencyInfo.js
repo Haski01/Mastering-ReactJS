a3b619da7f1a85a675b460ef19e2cd79400f2096
Asad <asadshah1066@gmail.com>
2025-06-22 16:40:12 +0530

    creating own hook for fetching data from currency api

---
+++ 06currencyConverter-Project/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((result) => setData(result[currency]));
+  }, [currency]);
+  // console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
+
+/* function useCurrencyInfo(currency) {
+  fetch(
+    `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data[currency]));
+}
+
+ useCurrencyInfo("usd");
+
+ */
