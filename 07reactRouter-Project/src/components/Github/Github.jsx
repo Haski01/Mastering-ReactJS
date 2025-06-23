a76707a1b0b4b5b8e164a28944a9675d0f2609b1
Asad <asadshah1066@gmail.com>
2025-06-23 18:20:10 +0530

    fetching github data

--- 07reactRouter-Project/src/components/Github/Github.jsx
+++ 07reactRouter-Project/src/components/Github/Github.jsx
@@ -15,10 +15,16 @@ function Github() {
   //   }, []);
 
   const data = useLoaderData();
+  console.log(data);
+
   return (
     <div className="flex justify-center p-4">
       <div className="text-center text-3xl bg-gray-600 text-white w-1/2 p-4">
-        GitHub Followers: {data.followers}
+        Name: {data.name}
+        <br />
+        UserName: {data.login}
+        <br />
+        Followers: {data.followers}
         <img src={data.avatar_url} alt="gitimg" className="w-72 mt-2" />
       </div>
     </div>
@@ -28,7 +34,8 @@ function Github() {
 export default Github;
 
 // optimize way to fetch data using useLoaderData hook
+
 export const githubData = async () => {
-  const response = await fetch("https://api.github.com/users/hiteshchoudhary");
+  const response = await fetch("https://api.github.com/users/Haski01");
   return response.json();
 };
