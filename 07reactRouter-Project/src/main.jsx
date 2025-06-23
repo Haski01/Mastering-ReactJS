e07fd96cc929e702adde13ea16751eebd6877d3e
Asad <asadshah1066@gmail.com>
2025-06-23 17:32:02 +0530

    routes

---
+++ 07reactRouter-Project/src/main.jsx
@@ -0,0 +1,44 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { About, Contact, Home, User } from "./components/index.js";
+
+// =>                     [Methods for routing]
+/*
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+    ],
+  },
+]);
+*/
+
+// another or optimize method for routing
+const route = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userId" element={<User />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={route} />
+  </StrictMode>
+);
