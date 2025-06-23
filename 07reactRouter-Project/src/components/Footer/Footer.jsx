50f608101306bae49a253e01d444867143f75062
Asad <asadshah1066@gmail.com>
2025-06-23 17:30:50 +0530

    React router components

---
+++ 07reactRouter-Project/src/components/Footer/Footer.jsx
@@ -0,0 +1,101 @@
+import { Link, NavLink } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white border-y">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <Link to="/" className="flex items-center">
+              <img
+                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                className="mr-3 h-16"
+                alt="Logo"
+              />
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Resources
+              </h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  {/* <Link to="/" className="hover:underline">
+                    Home
+                  </Link> */}
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      `${
+                        isActive ? "text-orange-700" : "text-gray-700"
+                      } block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  {/* <Link to="/about" className="hover:underline">
+                    About
+                  </Link> */}
+                  <NavLink
+                    to="/about"
+                    className={({ isActive }) =>
+                      ` ${
+                        isActive ? "text-orange-700" : "text-gray-700"
+                      } block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                    }
+                  >
+                    About
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Follow us
+              </h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <a
+                    href="#"
+                    className="hover:underline"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Github
+                  </a>
+                </li>
+                <li>
+                  <Link to="/" className="hover:underline">
+                    Discord
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Legal
+              </h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <Link to="#" className="hover:underline">
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#" className="hover:underline">
+                    Terms &amp; Conditions
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        
+      </div>
+    </footer>
+  );
+}
