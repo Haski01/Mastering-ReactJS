93ac2d4bca669004153ba3cd169100a1909c16d2
Asad <asadshah1066@gmail.com>
2025-07-08 17:45:41 +0530

    database ,storage and store congiguration

--- 11_MegaBlog-project/src/App.jsx
+++ 11_MegaBlog-project/src/App.jsx
@@ -1,11 +1,46 @@
-import "./App.css";
-import conf from "./env-variable-import/config";
+// import "./App.css";
+
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Outlet } from "react-router-dom";
+
+import authService from "./appwrite/authenticationService";
+import { Header, Footer } from "./components";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  return (
-    <>
-      <h1>blog with appwrite</h1>
-    </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        // userData ? dispatch(login(userData)) : dispatch(logout);
+
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  // condiotional rendering
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-300">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div className="text-center text-3xl bg-black text-white">
+      ......Loading
+    </div>
   );
 }
 
